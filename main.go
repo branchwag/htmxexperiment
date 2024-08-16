f9528549b1e86e0ca9efada3cc3001b676064e2e
LeAnne B. <branchleannelmb@gmail.com>
2024-08-16 19:13:13 -0400

    adds uploading functionality

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -159,6 +160,38 @@ func submitHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func uploadHandler( w http.ResponseWriter, r *http.Request) {
+	err :=r.ParseMultipartForm(10 << 20) //10MB limit
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Unable to get file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	out, err := os.Create(filepath.Join(".", "uploaded_file"))
+	if err != nil {
+		http.Error(w, "Unable to create file", http.StatusInternalServerError)
+		return
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		http.Error(w, "Unable to save file", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<p>File uploaded!</p>"))
+
+}
+
 
 func main() {
 	http.HandleFunc("/", indexHandler)
@@ -166,6 +199,7 @@ func main() {
 	http.HandleFunc("/otherpage", otherpageHandler)
 	http.HandleFunc("/submit", submitHandler)
 	http.HandleFunc("/test", testHandler)
+	http.HandleFunc("/upload", uploadHandler)
 
 	fmt.Println("Starting server on :4242")
 	if err := http.ListenAndServe(":4242", nil); err != nil {
